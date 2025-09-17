dee36b12db32eb26bd5f0cd8d751cb4118cfdf3e
ioannis kalafatas <ioanniskalafatas@ioanniss-MacBook-Pro.local>
2025-09-17 21:08:51 +0300

    [Added] improve a little bit the pool handle connection

--- src/broker/server.rs
+++ src/broker/server.rs
@@ -3,12 +3,11 @@ use crate::shards::{get_global_queue, init_global_queue};
 use std::collections::VecDeque;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::sync::Arc;
-use std::sync::{Condvar, Mutex};
+use std::sync::{Arc, Condvar, Mutex};
 use std::thread;
 
 pub struct ThreadPool {
-    _workers: Vec<thread::JoinHandle<()>>,
+    workers: Vec<thread::JoinHandle<()>>,
     queue: Arc<(Mutex<VecDeque<TcpStream>>, Condvar)>,
 }
 
@@ -28,15 +27,15 @@ impl ThreadPool {
                     }
                     q.pop_front().unwrap()
                 };
-                handle_client(stream, shard_count, get_global_queue());
+                let _ = std::panic::catch_unwind(|| {
+                    handle_client(stream, shard_count, get_global_queue());
+                });
             }));
         }
 
-        Self {
-            _workers: workers,
-            queue,
-        }
+        Self { workers, queue }
     }
+
     pub fn submit(&self, stream: TcpStream) {
         let (lock, cvar) = &*self.queue;
         let mut q = lock.lock().unwrap();
@@ -47,16 +46,14 @@ impl ThreadPool {
 
 pub fn run(addr: &str, shard_count: usize, pool_size: usize) -> std::io::Result<()> {
     init_global_queue(shard_count);
-
     let listener = TcpListener::bind(addr)?;
-    println!("Broker Listening on {}", addr);
+    println!("Broker listening on {}", addr);
 
     let pool = ThreadPool::new(pool_size, shard_count);
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(s) => pool.submit(s),
-            Err(e) => eprintln!("Failed to accept connection: {}", e),
+        if let Ok(s) = stream {
+            pool.submit(s);
         }
     }
     Ok(())
